2c9dbc2421b247f9b6abda50f031319fecba6e14
Victor Martinez <victormartinobre@hotmail.com>
2024-03-18 17:23:12 -0300

    feat: :construction: creating-components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './Home.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <header className="App-header">       
+      <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home/Home';
+import Footer from './Components/Footer/Footer';
+import NavBar from './Components/NavBar/NavBar'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Footer />
+    <Home />
+    <NavBar />
   </React.StrictMode>
 );
 
