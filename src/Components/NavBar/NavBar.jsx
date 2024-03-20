03a89ff65430df577389803313a8d58a0e17f3d8
VictormMartinez36 <victor.nobrega@sphereit.com.br>
2024-03-20 17:06:59 -0300

    update

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./NavBar.scss"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='divFooterMaster'>
+      <div className='divFooter'>
+        <img className='logo'
+         src="https://png.pngtree.com/png-vector/20221217/ourmid/pngtree-example-sample-grungy-stamp-vector-png-image_15560590.png" 
+         alt="Exemple"/>
+      </div>
+    </div>
   )
 }
 
