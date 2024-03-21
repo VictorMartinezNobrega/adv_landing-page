bf8199db5dac4e72b94048402505f1d1bed2d22a
Victor Martinez <victormartinobre@hotmail.com>
2024-03-21 09:51:34 -0300

    add-MaterialUI

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,9 +5,18 @@ const NavBar = () => {
   return (
     <div className='divFooterMaster'>
       <div className='divFooter'>
-        <img className='logo'
-         src="https://png.pngtree.com/png-vector/20221217/ourmid/pngtree-example-sample-grungy-stamp-vector-png-image_15560590.png" 
-         alt="Exemple"/>
+        <div className='div-logo'>
+          <img className='logo'
+          src="https://png.pngtree.com/png-vector/20221217/ourmid/pngtree-example-sample-grungy-stamp-vector-png-image_15560590.png" 
+          alt="Exemple"/>
+        </div>
+        <div className='div-options'>
+          <a className='btn-options'> Quem somos</a>
+          <a className='btn-options'>Atuação</a>
+          <a className='btn-options'>Time</a>
+          <a className='btn-options'>Escritórios</a>
+          <a className='btn-options'>Blog</a>
+        </div>
       </div>
     </div>
   )
