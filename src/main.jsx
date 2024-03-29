e9847f4276f7a6706a9363ff0bb98d2e351674bb
Victor Martinez <victormartinobre@hotmail.com>
2024-03-28 22:52:12 -0300

    ;-; add Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import Home from './Pages/Home/Home';
 import Footer from './Components/Footer/Footer';
