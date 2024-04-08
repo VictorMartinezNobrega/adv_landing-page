37bdb7c0b282fcca0c118268b0b2d6bae4b7d9c6
Victor Martinez <victormartinobre@hotmail.com>
2024-04-08 12:24:29 -0300

    update

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,17 +2,14 @@ import "./NavBar.css"
 
 const NavBar = () => {
   return (
-    <div className='.w-auto'>
-      <div className='.flex .bg-gold .flex-col .items-center'>
-        <div className='.h-24 .inline-flex'>
-          <img 
-                style={{
-                  height: 100
-                }}
+    <div className='w-auto'>
+      <div className='flex bg-blue-dark flex-col items-center'>
+        <div className='h-24 inline-flex'>
+          <img className="h-24"
           src="https://png.pngtree.com/png-vector/20221217/ourmid/pngtree-example-sample-grungy-stamp-vector-png-image_15560590.png" 
           alt="Exemple"/>
         </div>
-        <div className='div-options'>
+        <div className='flex flex-row py-1 text-xs' >
           <a className='btn-options'> Quem somos</a>
           <a className='btn-options'>Atuação</a>
           <a className='btn-options'>Time</a>
